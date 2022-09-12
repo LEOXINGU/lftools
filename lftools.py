8029735335306246b16c792dc99cf3bae209be8c
Leandro França <geoleandro.franca@gmail.com>
2022-09-12 01:11:35 -0300

    Update lftools.py

--- lftools.py
+++ lftools.py
@@ -43,7 +43,7 @@ from .expressions import *
 
 exprs = (coord2inom, fieldstat, dd2dms, projectCRS, layerCRS, magneticdec,
          dms2dd, scalefactor, zonehemisf, deedtable, inom2mi, meridianconv,
-         removespetialchar, deedtable2, deedtable3, areaLTP, deedtext)
+         removespetialchar, deedtable2, deedtable3, areaLTP, deedtext, geoneighbors)
 
 cmd_folder = os.path.split(inspect.getfile(inspect.currentframe()))[0]
 
