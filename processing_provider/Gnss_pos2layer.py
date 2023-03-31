8a65f6d72367df62349518473bf393a8ff1ac096
Leandro França <geoleandro.franca@gmail.com>
2023-03-31 07:29:20 -0300

    Update Gnss_pos2layer.py

--- processing_provider/Gnss_pos2layer.py
+++ processing_provider/Gnss_pos2layer.py
@@ -88,7 +88,7 @@ class Pos2layer(QgsProcessingAlgorithm):
         return 'gnss'
 
     def tags(self):
-        return self.tr('gps,position,ibge,.pos,rtklib,ppp,navigation,satellites,surveying,glonass,beidou,compass,galileu,track,kinematic,rtk,ntrip,static').split(',')
+        return self.tr('gps,position,ibge,.pos,rtklib,ppp,navigation,satellites,rinex,surveying,glonass,beidou,compass,galileu,track,kinematic,rtk,ntrip,static').split(',')
 
     def icon(self):
         return QIcon(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images/satellite.png'))
