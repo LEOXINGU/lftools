bf418d5efef1961b8c23c739daa5b08d251f4fe8
Leandro França <geoleandro.franca@gmail.com>
2023-11-15 12:01:57 -0300

    Update expressions.py

--- expressions.py
+++ expressions.py
@@ -755,7 +755,7 @@ def deedtable(layer_name, ini, fim, titulo, fontsize, feature, parent):
       <title>''' + tr('Synthetic deed description', str2HTML('Memorial Sintético')) + '''</title>    </head>
     <body>
     <table
-    style="text-align: center; width: 100%; font-size: [FONTSIZE]px; font-family: Arial; border: medium none; border-collapse: collapse;"
+    style="text-align: center; width: 100%; font-size: [FONTSIZE]px;  border: medium none; border-collapse: collapse;"
     border="1" cellpadding="0" cellspacing="0">
     <tbody>
     [CABECALHO]
@@ -989,7 +989,7 @@ def deedtable2(prefix, titulo, decimal, fontsize, layer_name, tipo, azimuteDist,
           <title>''' + tr('Synthetic deed description', str2HTML('Memorial Sintético')) + '''</title>    </head>
         <body>
         <table
-        style="text-align: center; width: 100%; font-size: [FONTSIZE]px; font-family: Arial; border: medium none; border-collapse: collapse;"
+        style="text-align: center; width: 100%; font-size: [FONTSIZE]px;  border: medium none; border-collapse: collapse;"
         border="1" cellpadding="0" cellspacing="0">
         <tbody>
         [CABECALHO]
@@ -1312,7 +1312,7 @@ def deedtable3(prefix, titulo, decimal, fontsize, layer_name, tipo, azimuteDist,
           <title>''' + tr('Synthetic deed description', str2HTML('Memorial Sintético')) + '''</title>    </head>
         <body>
         <table class="MsoTableGrid"
-        style="text-align: center; width: 100%; font-size: [FONTSIZE]px; font-family: Arial; border: medium none ; border-collapse: collapse;"
+        style="text-align: center; width: 100%; font-size: [FONTSIZE]px;  border: medium none ; border-collapse: collapse;"
         border="1" cellpadding="0" cellspacing="0">
         <tbody>
         [CABECALHO]
@@ -1705,8 +1705,8 @@ def deedtext(layer_name, description, estilo, prefix, decimal, fontsize, feature
             descr_pnt_ini = str(descr_pnt_ini)
 
             # conteudo do memorial
-            text_ini = tr('<p style="text-align: justify; font-size: [FONTSIZE]px; font-family: Arial;">The description of this perimeter begins at the vertex <b>[Vn]</b>, with coordinates ' + estilo_vertices + ', [descr_pnt_ini] from this, with the following flat azimuths and distances: ',
-                          '<p style="text-align: justify; font-size: [FONTSIZE]px; font-family: Arial;">Inicia-se a descrição deste perímetro no vértice <b>[Vn]</b>, de coordenadas ' + estilo_vertices + ', [descr_pnt_ini] deste, segue com os seguintes azimutes planos e distâncias: ')
+            text_ini = tr('<p style="text-align: justify; font-size: [FONTSIZE]px; ">The description of this perimeter begins at the vertex <b>[Vn]</b>, with coordinates ' + estilo_vertices + ', [descr_pnt_ini] from this, with the following flat azimuths and distances: ',
+                          '<p style="text-align: justify; font-size: [FONTSIZE]px; ">Inicia-se a descrição deste perímetro no vértice <b>[Vn]</b>, de coordenadas ' + estilo_vertices + ', [descr_pnt_ini] deste, segue com os seguintes azimutes planos e distâncias: ')
 
             text_meio = tr('[Azn] and [Dn]m up to the vertex <b>[Vn]</b>, with coordinates ' + estilo_vertices + ', ',
                            '[Azn] e [Dn]m até o vértice <b>[Vn]</b>, de coordenadas ' + estilo_vertices + ', ')
@@ -1786,8 +1786,8 @@ def deedtext(layer_name, description, estilo, prefix, decimal, fontsize, feature
         except: # >>>>>>>>>>>>>>>>> puxar nome dos confrontantes
 
             # conteudo do memorial
-            text_ini = tr('<p style="text-align: justify; font-size: [FONTSIZE]px; font-family: Arial;">The description of this perimeter begins at the vertex <b>[Vn]</b>, with coordinates ' + estilo_vertices + ', ',
-                          '<p style="text-align: justify; font-size: [FONTSIZE]px; font-family: Arial;">Inicia-se a descrição deste perímetro no vértice <b>[Vn]</b>, de coordenadas ' + estilo_vertices + ', ')
+            text_ini = tr('<p style="text-align: justify; font-size: [FONTSIZE]px; ">The description of this perimeter begins at the vertex <b>[Vn]</b>, with coordinates ' + estilo_vertices + ', ',
+                          '<p style="text-align: justify; font-size: [FONTSIZE]px; ">Inicia-se a descrição deste perímetro no vértice <b>[Vn]</b>, de coordenadas ' + estilo_vertices + ', ')
 
             text_meio1 = tr('from this, it continues adjoining with [ADJOINER], with the following flat azimuths and distances: [Azn] and [Dn]m up to the vertex <b>[Vn]</b>, with coordinates ' + estilo_vertices + ', ',
                             'deste, segue confrontando com [ADJOINER], com os seguintes azimutes planos e distâncias: [Azn] e [Dn]m até o vértice <b>[Vn]</b>, de coordenadas ' + estilo_vertices + ', ')
