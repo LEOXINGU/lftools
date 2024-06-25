2015414213612b890325924d2dbbb0999a44dd80
Leandro França <geoleandro.franca@gmail.com>
2024-06-25 16:17:11 -0300

    Update Post_Restore.py

--- processing_provider/Post_Restore.py
+++ processing_provider/Post_Restore.py
@@ -30,6 +30,7 @@ from qgis.core import (QgsProcessing,
                        QgsCoordinateReferenceSystem)
 
 from lftools.geocapt.imgs import Imgs
+from lftools.translations.translate import translate
 import os
 from qgis.PyQt.QtGui import QIcon
 
@@ -37,18 +38,8 @@ class Restore(QgsProcessingAlgorithm):
 
     LOC = QgsApplication.locale()[:2]
 
-    def translate(self, string):
-        return QCoreApplication.translate('Processing', string)
-
     def tr(self, *string):
-        # Traduzir para o portugês: arg[0] - english (translate), arg[1] - português
-        if self.LOC == 'pt':
-            if len(string) == 2:
-                return string[1]
-            else:
-                return self.translate(string[0])
-        else:
-            return self.translate(string[0])
+        return translate(string, self.LOC)
 
     def createInstance(self):
         return Restore()
