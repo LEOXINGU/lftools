25e11a5d2185e42d1d83a117d644450add2f06ef
Leandro França <geoleandro.franca@gmail.com>
2024-06-25 16:14:54 -0300

    Update Vect_LinesToPolygon.py

--- processing_provider/Vect_LinesToPolygon.py
+++ processing_provider/Vect_LinesToPolygon.py
@@ -39,6 +39,7 @@ from qgis.core import (QgsApplication,
                        QgsCoordinateTransform,
                        QgsProcessingParameterFeatureSink)
 from lftools.geocapt.imgs import Imgs
+from lftools.translations.translate import translate
 import os
 from qgis.PyQt.QtGui import QIcon
 from lftools.geocapt.cartography import geom2PointList
@@ -47,18 +48,8 @@ class LinesToPolygon(QgsProcessingAlgorithm):
 
     LOC = QgsApplication.locale()[:2]
 
-    def translate(self, string):
-        return QCoreApplication.translate('Processing', string)
-
     def tr(self, *string):
-        # Traduzir para o portugês: arg[0] - english (translate), arg[1] - português
-        if self.LOC == 'pt':
-            if len(string) == 2:
-                return string[1]
-            else:
-                return self.translate(string[0])
-        else:
-            return self.translate(string[0])
+        return translate(string, self.LOC)
 
     def createInstance(self):
         return LinesToPolygon()
