328c6a728f6c86cb57516f554ffca3db7ca1483c
Leandro França <geoleandro.franca@gmail.com>
2024-06-25 16:16:49 -0300

    Update Rast_extractRasterBand.py

--- processing_provider/Rast_extractRasterBand.py
+++ processing_provider/Rast_extractRasterBand.py
@@ -47,6 +47,7 @@ from qgis.core import (QgsProcessing,
 
 from osgeo import osr, gdal_array, gdal #https://gdal.org/python/
 from lftools.geocapt.imgs import Imgs
+from lftools.translations.translate import translate
 import os
 from qgis.PyQt.QtGui import QIcon
 
@@ -54,18 +55,8 @@ class ExtractRasterBand(QgsProcessingAlgorithm):
 
     LOC = QgsApplication.locale()[:2]
 
-    def translate(self, string):
-        return QCoreApplication.translate('Processing', string)
-
     def tr(self, *string):
-        # Traduzir para o portugês: arg[0] - english (translate), arg[1] - português
-        if self.LOC == 'pt':
-            if len(string) == 2:
-                return string[1]
-            else:
-                return self.translate(string[0])
-        else:
-            return self.translate(string[0])
+        return translate(string, self.LOC)
 
     def createInstance(self):
         return ExtractRasterBand()
