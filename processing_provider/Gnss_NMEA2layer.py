317004936f5afd3da6c166e371d58249265bc25d
Leandro França <geoleandro.franca@gmail.com>
2024-06-25 16:17:40 -0300

    Update Gnss_NMEA2layer.py

--- processing_provider/Gnss_NMEA2layer.py
+++ processing_provider/Gnss_NMEA2layer.py
@@ -45,6 +45,7 @@ from qgis.core import (QgsProcessing,
                        QgsCoordinateReferenceSystem)
 
 from lftools.geocapt.imgs import Imgs
+from lftools.translations.translate import translate
 from lftools.geocapt.cartography import raioMedioGauss
 import numpy as np
 import datetime
@@ -57,18 +58,8 @@ class NMEA2layer(QgsProcessingAlgorithm):
 
     LOC = QgsApplication.locale()[:2]
 
-    def translate(self, string):
-        return QCoreApplication.translate('Processing', string)
-
     def tr(self, *string):
-        # Traduzir para o portugês: arg[0] - english (translate), arg[1] - português
-        if self.LOC == 'pt':
-            if len(string) == 2:
-                return string[1]
-            else:
-                return self.translate(string[0])
-        else:
-            return self.translate(string[0])
+        return translate(string, self.LOC)
 
     def createInstance(self):
         return NMEA2layer()
