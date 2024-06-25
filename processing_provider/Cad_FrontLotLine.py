63a595e74dbdb95ac8cc94f37597faaa477b5899
Leandro França <geoleandro.franca@gmail.com>
2024-06-25 16:18:53 -0300

    Update Cad_FrontLotLine.py

--- processing_provider/Cad_FrontLotLine.py
+++ processing_provider/Cad_FrontLotLine.py
@@ -32,6 +32,7 @@ from qgis.core import (QgsApplication,
                        QgsProcessingParameterFeatureSource,
                        QgsProcessingParameterFeatureSink)
 from lftools.geocapt.imgs import Imgs
+from lftools.translations.translate import translate
 from lftools.geocapt.cartography import OrientarPoligono
 import numpy as np
 import os
@@ -41,18 +42,8 @@ class FrontLotLine(QgsProcessingAlgorithm):
 
     LOC = QgsApplication.locale()[:2]
 
-    def translate(self, string):
-        return QCoreApplication.translate('Processing', string)
-
     def tr(self, *string):
-        # Traduzir para o portugês: arg[0] - english (translate), arg[1] - português
-        if self.LOC == 'pt':
-            if len(string) == 2:
-                return string[1]
-            else:
-                return self.translate(string[0])
-        else:
-            return self.translate(string[0])
+        return translate(string, self.LOC)
 
     def createInstance(self):
         return FrontLotLine()
