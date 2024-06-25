c23876ddfdd7cf43248729ee72d05d7726269bf3
Leandro França <geoleandro.franca@gmail.com>
2024-06-25 16:16:06 -0300

    Update Relief_DEM2txt.py

--- processing_provider/Relief_DEM2txt.py
+++ processing_provider/Relief_DEM2txt.py
@@ -52,6 +52,7 @@ import numpy as np
 from pyproj.crs import CRS
 from math import floor, ceil
 from lftools.geocapt.imgs import Imgs
+from lftools.translations.translate import translate
 from lftools.geocapt.dip import Interpolar
 import os
 from qgis.PyQt.QtGui import QIcon
@@ -60,18 +61,8 @@ class DEM2txt(QgsProcessingAlgorithm):
 
     LOC = QgsApplication.locale()[:2]
 
-    def translate(self, string):
-        return QCoreApplication.translate('Processing', string)
-
     def tr(self, *string):
-        # Traduzir para o portugês: arg[0] - english (translate), arg[1] - português
-        if self.LOC == 'pt':
-            if len(string) == 2:
-                return string[1]
-            else:
-                return self.translate(string[0])
-        else:
-            return self.translate(string[0])
+        return translate(string, self.LOC)
 
     def createInstance(self):
         return DEM2txt()
