011360b7f399f8b2da0ce4f30529f7e94ae4c54b
Leandro França <geoleandro.franca@gmail.com>
2024-06-25 16:17:50 -0300

    Update Easy_coord2layer.py

--- processing_provider/Easy_coord2layer.py
+++ processing_provider/Easy_coord2layer.py
@@ -19,6 +19,7 @@ from PyQt5.QtCore import QCoreApplication, QVariant
 from qgis.core import *
 import processing
 from lftools.geocapt.imgs import Imgs
+from lftools.translations.translate import translate
 from lftools.geocapt.topogeo import dms2dd
 import os
 from qgis.PyQt.QtGui import QIcon
@@ -34,18 +35,8 @@ class CoordinatesToLayer(QgsProcessingAlgorithm):
     LAYER = 'LAYER'
     LOC = QgsApplication.locale()[:2]
 
-    def translate(self, string):
-        return QCoreApplication.translate('Processing', string)
-
     def tr(self, *string):
-        # Traduzir para o portugês: arg[0] - english (translate), arg[1] - português
-        if self.LOC == 'pt':
-            if len(string) == 2:
-                return string[1]
-            else:
-                return self.translate(string[0])
-        else:
-            return self.translate(string[0])
+        return translate(string, self.LOC)
 
     def createInstance(self):
         return CoordinatesToLayer()
