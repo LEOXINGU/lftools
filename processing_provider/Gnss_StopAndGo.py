7401e399fcda9ebcf93c75f3acf1f3ac95d7ba63
Leandro França <geoleandro.franca@gmail.com>
2024-06-25 16:17:32 -0300

    Update Gnss_StopAndGo.py

--- processing_provider/Gnss_StopAndGo.py
+++ processing_provider/Gnss_StopAndGo.py
@@ -46,6 +46,7 @@ from qgis.core import (QgsProcessing,
                        QgsCoordinateReferenceSystem)
 
 from lftools.geocapt.imgs import Imgs
+from lftools.translations.translate import translate
 from lftools.geocapt.cartography import raioMedioGauss
 import numpy as np
 from pyproj.crs import CRS
@@ -59,18 +60,8 @@ class StopAndGo(QgsProcessingAlgorithm):
 
     LOC = QgsApplication.locale()[:2]
 
-    def translate(self, string):
-        return QCoreApplication.translate('Processing', string)
-
     def tr(self, *string):
-        # Traduzir para o portugês: arg[0] - english (translate), arg[1] - português
-        if self.LOC == 'pt':
-            if len(string) == 2:
-                return string[1]
-            else:
-                return self.translate(string[0])
-        else:
-            return self.translate(string[0])
+        return translate(string, self.LOC)
 
     def createInstance(self):
         return StopAndGo()
