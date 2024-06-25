a59bb08de696779ad1406a7c69d9729d1a46e2ff
Leandro França <geoleandro.franca@gmail.com>
2024-06-25 16:15:02 -0300

    Update Vect_DirectionalMerge.py

--- processing_provider/Vect_DirectionalMerge.py
+++ processing_provider/Vect_DirectionalMerge.py
@@ -33,6 +33,7 @@ from qgis.core import (QgsApplication,
                        QgsProcessingParameterFeatureSink)
 from math import atan2, degrees, fabs
 from lftools.geocapt.imgs import Imgs
+from lftools.translations.translate import translate
 import os
 from qgis.PyQt.QtGui import QIcon
 
@@ -40,18 +41,8 @@ class DirectionalMerge(QgsProcessingAlgorithm):
 
     LOC = QgsApplication.locale()[:2]
 
-    def translate(self, string):
-        return QCoreApplication.translate('Processing', string)
-
     def tr(self, *string):
-        # Traduzir para o portugês: arg[0] - english (translate), arg[1] - português
-        if self.LOC == 'pt':
-            if len(string) == 2:
-                return string[1]
-            else:
-                return self.translate(string[0])
-        else:
-            return self.translate(string[0])
+        return translate(string, self.LOC)
 
     def createInstance(self):
         return DirectionalMerge()
