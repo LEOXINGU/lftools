289dcb59e6cbb7f55dfc3c638d08ec3b8d3ec4af
Leandro França <geoleandro.franca@gmail.com>
2024-06-25 16:15:00 -0300

    Update Vect_ExtendLines.py

--- processing_provider/Vect_ExtendLines.py
+++ processing_provider/Vect_ExtendLines.py
@@ -37,6 +37,7 @@ import numpy as np
 from numpy.linalg import norm
 from pyproj.crs import CRS
 from lftools.geocapt.imgs import Imgs
+from lftools.translations.translate import translate
 import os
 from qgis.PyQt.QtGui import QIcon
 
@@ -44,18 +45,8 @@ class ExtendLines(QgsProcessingAlgorithm):
 
     LOC = QgsApplication.locale()[:2]
 
-    def translate(self, string):
-        return QCoreApplication.translate('Processing', string)
-
     def tr(self, *string):
-        # Traduzir para o portugês: arg[0] - english (translate), arg[1] - português
-        if self.LOC == 'pt':
-            if len(string) == 2:
-                return string[1]
-            else:
-                return self.translate(string[0])
-        else:
-            return self.translate(string[0])
+        return translate(string, self.LOC)
 
     def createInstance(self):
         return ExtendLines()
