b5b695a2edab92ef99e3a789866a08cb4d9bb898
Leandro França <geoleandro.franca@gmail.com>
2024-06-25 16:17:58 -0300

    Update Drone_saveAsJPEG.py

--- processing_provider/Drone_saveAsJPEG.py
+++ processing_provider/Drone_saveAsJPEG.py
@@ -52,6 +52,7 @@ from math import floor, ceil
 import numpy as np
 from PIL import Image
 from lftools.geocapt.imgs import Imgs
+from lftools.translations.translate import translate
 import os
 from qgis.PyQt.QtGui import QIcon
 
@@ -59,18 +60,8 @@ class SaveAsJPEG(QgsProcessingAlgorithm):
 
     LOC = QgsApplication.locale()[:2]
 
-    def translate(self, string):
-        return QCoreApplication.translate('Processing', string)
-
     def tr(self, *string):
-        # Traduzir para o portugês: arg[0] - english (translate), arg[1] - português
-        if self.LOC == 'pt':
-            if len(string) == 2:
-                return string[1]
-            else:
-                return self.translate(string[0])
-        else:
-            return self.translate(string[0])
+        return translate(string, self.LOC)
 
     def createInstance(self):
         return SaveAsJPEG()
