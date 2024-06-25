92ba9793bcf15265ba4e799b9e4521baae2605d4
Leandro França <geoleandro.franca@gmail.com>
2024-06-25 16:15:49 -0300

    Update Stat_nearestPoints.py

--- processing_provider/Stat_nearestPoints.py
+++ processing_provider/Stat_nearestPoints.py
@@ -35,6 +35,7 @@ from qgis.core import (QgsProcessing,
                        QgsCoordinateReferenceSystem)
 import numpy as np
 from lftools.geocapt.imgs import Imgs
+from lftools.translations.translate import translate
 from lftools.geocapt.topogeo import meters2degrees, degrees2meters
 import os
 from qgis.PyQt.QtGui import QIcon
@@ -44,18 +45,8 @@ class NearestPoints(QgsProcessingAlgorithm):
 
     LOC = QgsApplication.locale()[:2]
 
-    def translate(self, string):
-        return QCoreApplication.translate('Processing', string)
-
     def tr(self, *string):
-        # Traduzir para o portugês: arg[0] - english (translate), arg[1] - português
-        if self.LOC == 'pt':
-            if len(string) == 2:
-                return string[1]
-            else:
-                return self.translate(string[0])
-        else:
-            return self.translate(string[0])
+        return translate(string, self.LOC)
 
     def createInstance(self):
         return NearestPoints()
