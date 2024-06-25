1cc371427b452b5c329512ef45f4502e5f3a92a7
Leandro França <geoleandro.franca@gmail.com>
2024-06-25 16:16:58 -0300

    Update Rast_compressJPEG.py

--- processing_provider/Rast_compressJPEG.py
+++ processing_provider/Rast_compressJPEG.py
@@ -48,6 +48,7 @@ from qgis.core import (QgsProcessing,
 
 from osgeo import osr, gdal_array, gdal #https://gdal.org/python/
 from lftools.geocapt.imgs import Imgs
+from lftools.translations.translate import translate
 from qgis.PyQt.QtGui import QIcon
 import os
 
@@ -55,18 +56,8 @@ class CompressJPEG(QgsProcessingAlgorithm):
 
     LOC = QgsApplication.locale()[:2]
 
-    def translate(self, string):
-        return QCoreApplication.translate('Processing', string)
-
     def tr(self, *string):
-        # Traduzir para o portugês: arg[0] - english (translate), arg[1] - português
-        if self.LOC == 'pt':
-            if len(string) == 2:
-                return string[1]
-            else:
-                return self.translate(string[0])
-        else:
-            return self.translate(string[0])
+        return translate(string, self.LOC)
 
     def createInstance(self):
         return CompressJPEG()
