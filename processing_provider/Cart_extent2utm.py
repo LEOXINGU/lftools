331421b5a40c05543010bfb0aa4cbc7025541d01
Leandro França <geoleandro.franca@gmail.com>
2024-06-25 16:18:43 -0300

    Update Cart_extent2utm.py

--- processing_provider/Cart_extent2utm.py
+++ processing_provider/Cart_extent2utm.py
@@ -18,6 +18,7 @@ __copyright__ = '(C) 2019, Leandro França'
 from PyQt5.QtCore import QCoreApplication, QVariant
 from qgis.core import *
 from lftools.geocapt.imgs import Imgs
+from lftools.translations.translate import translate
 from lftools.geocapt.cartography import *
 import os
 from qgis.PyQt.QtGui import QIcon
@@ -37,18 +38,8 @@ class Extent2UTMGrid(QgsProcessingAlgorithm):
 
     LOC = QgsApplication.locale()[:2]
 
-    def translate(self, string):
-        return QCoreApplication.translate('Processing', string)
-
     def tr(self, *string):
-        # Traduzir para o portugês: arg[0] - english (translate), arg[1] - português
-        if self.LOC == 'pt':
-            if len(string) == 2:
-                return string[1]
-            else:
-                return self.translate(string[0])
-        else:
-            return self.translate(string[0])
+        return translate(string, self.LOC)
 
     def createInstance(self):
         return Extent2UTMGrid()
