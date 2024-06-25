f7ef26d2fd8b49ff124797b354456eda592bd995
Leandro França <geoleandro.franca@gmail.com>
2024-06-25 16:16:31 -0300

    Update Rast_loadRasterByLocation.py

--- processing_provider/Rast_loadRasterByLocation.py
+++ processing_provider/Rast_loadRasterByLocation.py
@@ -49,6 +49,7 @@ from qgis.core import (QgsProcessing,
 
 from osgeo import osr, gdal_array, gdal #https://gdal.org/python/
 from lftools.geocapt.imgs import Imgs
+from lftools.translations.translate import translate
 from lftools.geocapt.cartography import reprojectPoints
 import os, shutil
 import numpy as np
@@ -58,18 +59,8 @@ class LoadRasterByLocation(QgsProcessingAlgorithm):
 
     LOC = QgsApplication.locale()[:2]
 
-    def translate(self, string):
-        return QCoreApplication.translate('Processing', string)
-
     def tr(self, *string):
-        # Traduzir para o portugês: arg[0] - english (translate), arg[1] - português
-        if self.LOC == 'pt':
-            if len(string) == 2:
-                return string[1]
-            else:
-                return self.translate(string[0])
-        else:
-            return self.translate(string[0])
+        return translate(string, self.LOC)
 
     def createInstance(self):
         return LoadRasterByLocation()
