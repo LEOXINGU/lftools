1decbc9af3d18e778a1e8ff46ecfed3deb87a31b
Leandro França <geoleandro.franca@gmail.com>
2024-06-25 16:16:29 -0300

    Update Rast_mosaicRaster.py

--- processing_provider/Rast_mosaicRaster.py
+++ processing_provider/Rast_mosaicRaster.py
@@ -54,6 +54,7 @@ import numpy as np
 from pyproj.crs import CRS
 from math import floor, ceil
 from lftools.geocapt.imgs import Imgs
+from lftools.translations.translate import translate
 from lftools.geocapt.dip import Interpolar
 import os
 from qgis.PyQt.QtGui import QIcon
@@ -62,18 +63,8 @@ class MosaicRaster(QgsProcessingAlgorithm):
 
     LOC = QgsApplication.locale()[:2]
 
-    def translate(self, string):
-        return QCoreApplication.translate('Processing', string)
-
     def tr(self, *string):
-        # Traduzir para o portugês: arg[0] - english (translate), arg[1] - português
-        if self.LOC == 'pt':
-            if len(string) == 2:
-                return string[1]
-            else:
-                return self.translate(string[0])
-        else:
-            return self.translate(string[0])
+        return translate(string, self.LOC)
 
     def createInstance(self):
         return MosaicRaster()
