82219e655477d1adc7c6d94ae929432a3d5dae0a
Leandro França <geoleandro.franca@gmail.com>
2024-06-25 15:54:56 -0300

    Update Vect_PolygonAngles.py

--- processing_provider/Vect_PolygonAngles.py
+++ processing_provider/Vect_PolygonAngles.py
@@ -23,6 +23,7 @@ import numpy as np
 import math
 from numpy import floor
 from lftools.geocapt.imgs import Imgs
+from lftools.translations.translate import translate
 from lftools.geocapt.topogeo import azimute, dd2dms
 from lftools.geocapt.cartography import areaGauss
 import os
@@ -35,18 +36,8 @@ class CalculatePolygonAngles(QgsProcessingAlgorithm):
     ANGLES = 'ANGLES'
     LOC = QgsApplication.locale()[:2]
 
-    def translate(self, string):
-        return QCoreApplication.translate('Processing', string)
-
     def tr(self, *string):
-        # Traduzir para o portugês: arg[0] - english (translate), arg[1] - português
-        if self.LOC == 'pt':
-            if len(string) == 2:
-                return string[1]
-            else:
-                return self.translate(string[0])
-        else:
-            return self.translate(string[0])
+        return translate(string, self.LOC)
 
     def createInstance(self):
         return CalculatePolygonAngles()
