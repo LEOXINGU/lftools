3b856755303329dfa1f454ea6df0c17e184ffc30
Leandro França <geoleandro.franca@gmail.com>
2024-06-25 16:16:11 -0300

    Update Reamb_KmlPhotos.py

--- processing_provider/Reamb_KmlPhotos.py
+++ processing_provider/Reamb_KmlPhotos.py
@@ -34,6 +34,7 @@ from qgis.core import (QgsApplication,
                        QgsProcessingParameterFeatureSink)
 
 from lftools.geocapt.imgs import Imgs
+from lftools.translations.translate import translate
 import os
 from qgis.PyQt.QtGui import QIcon
 from lftools.geocapt.imgs import img2html_resized
@@ -43,18 +44,8 @@ class KmlPhotos(QgsProcessingAlgorithm):
 
     LOC = QgsApplication.locale()[:2]
 
-    def translate(self, string):
-        return QCoreApplication.translate('Processing', string)
-
     def tr(self, *string):
-        # Traduzir para o portugês: arg[0] - english (translate), arg[1] - português
-        if self.LOC == 'pt':
-            if len(string) == 2:
-                return string[1]
-            else:
-                return self.translate(string[0])
-        else:
-            return self.translate(string[0])
+        return translate(string, self.LOC)
 
     def createInstance(self):
         return KmlPhotos()
