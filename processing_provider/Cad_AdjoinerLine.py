c2b1d322eb83706ddaec3177a8476f193975ee55
Leandro França <geoleandro.franca@gmail.com>
2024-06-25 16:18:58 -0300

    Update Cad_AdjoinerLine.py

--- processing_provider/Cad_AdjoinerLine.py
+++ processing_provider/Cad_AdjoinerLine.py
@@ -35,6 +35,7 @@ from qgis.core import (QgsApplication,
                        QgsProcessingParameterFeatureSource,
                        QgsProcessingParameterFeatureSink)
 from lftools.geocapt.imgs import Imgs
+from lftools.translations.translate import translate
 from lftools.geocapt.cartography import OrientarPoligono
 import numpy as np
 import os
@@ -44,18 +45,8 @@ class AdjoinerLine(QgsProcessingAlgorithm):
 
     LOC = QgsApplication.locale()[:2]
 
-    def translate(self, string):
-        return QCoreApplication.translate('Processing', string)
-
     def tr(self, *string):
-        # Traduzir para o portugês: arg[0] - english (translate), arg[1] - português
-        if self.LOC == 'pt':
-            if len(string) == 2:
-                return string[1]
-            else:
-                return self.translate(string[0])
-        else:
-            return self.translate(string[0])
+        return translate(string, self.LOC)
 
     def createInstance(self):
         return AdjoinerLine()
