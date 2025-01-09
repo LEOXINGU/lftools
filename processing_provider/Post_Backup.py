f64d79c733d227ff45d082b2b521b4e6ae2ed47e
Leandro França <geoleandro.franca@gmail.com>
2025-01-09 12:57:05 -0300

    Update Post_Backup.py

--- processing_provider/Post_Backup.py
+++ processing_provider/Post_Backup.py
@@ -86,7 +86,7 @@ class Backup(QgsProcessingAlgorithm):
     USER = 'USER'
     PORT = 'PORT'
     VERSION = 'VERSION'
-    versions = ['9.5', '9.6', '10', '11', '12', '13', '14', '15', '16']
+    versions = ['9.5', '9.6', '10', '11', '12', '13', '14', '15', '16', '17']
 
     def initAlgorithm(self, config=None):
         # INPUT
