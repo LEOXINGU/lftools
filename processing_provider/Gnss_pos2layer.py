9722bdf27525847d45363aad69959e3b62ad508a
Leandro França <geoleandro.franca@gmail.com>
2025-02-07 14:21:52 -0300

    Update Gnss_pos2layer.py

--- processing_provider/Gnss_pos2layer.py
+++ processing_provider/Gnss_pos2layer.py
@@ -358,7 +358,7 @@ Tipos:
 
         if saida == 1:
             if model_vel > 0:
-                vlat, vlon = vemos(lat, lon, ['vemos2009','vemos2017'][model_vel-1])
+                vlat, vlon = vemos(lat, lon, ['vemos2009','vemos2017','vemos2022'][model_vel-1])
                 delta_tempo = datetime.strptime(datahora, "%Y-%m-%d %H:%M:%S") - datetime.strptime('2000-04-24 12:00:00', "%Y-%m-%d %H:%M:%S")
                 anos = delta_tempo.days/365.25
                 dLat = meters2degrees(vlat*anos, lat, QgsCoordinateReferenceSystem('EPSG:4674'))
