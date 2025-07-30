4d2287ffc15593c2c81cd673a63eafc7b5d7b07d
Leandro França <geoleandro.franca@gmail.com>
2025-07-30 12:41:54 -0300

    Update Rast_fillRasterwithPatches.py

--- processing_provider/Rast_fillRasterwithPatches.py
+++ processing_provider/Rast_fillRasterwithPatches.py
@@ -77,7 +77,7 @@ class FillRasterwithPatches(QgsProcessingAlgorithm):
         return 'raster'
 
     def tags(self):
-        return self.tr('fill,hole,raster,cloud,remove,drone,patch').split(',')
+        return 'GeoOne,fill,hole,raster,cloud,remove,drone,patch'.split(',')
 
     def icon(self):
         return QIcon(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images/raster.png'))
