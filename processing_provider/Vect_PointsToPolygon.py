5afc47524dd9956e6452913dc85c99740440e38f
Leandro França <geoleandro.franca@gmail.com>
2025-07-30 12:42:28 -0300

    Update Vect_PointsToPolygon.py

--- processing_provider/Vect_PointsToPolygon.py
+++ processing_provider/Vect_PointsToPolygon.py
@@ -66,7 +66,7 @@ class PointsToPolygon(QgsProcessingAlgorithm):
         return 'vector'
 
     def tags(self):
-        return self.tr('sequence,points,pontos,order,ordenar,orientar,polygon,polígono').split(',')
+        return 'GeoOne,sequence,points,pontos,order,ordenar,orientar,polygon,polígono'.split(',')
 
     def icon(self):
         return QIcon(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images/vetor.png'))
