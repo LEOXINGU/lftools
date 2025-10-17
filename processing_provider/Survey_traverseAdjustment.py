173a67c6c8b57323d80b87e31ffb532cbef66629
Leandro França <geoleandro.franca@gmail.com>
2025-10-17 17:27:45 -0300

    Update Survey_traverseAdjustment.py

--- processing_provider/Survey_traverseAdjustment.py
+++ processing_provider/Survey_traverseAdjustment.py
@@ -15,7 +15,7 @@ __author__ = 'Leandro França'
 __date__ = '2019-11-17'
 __copyright__ = '(C) 2019, Leandro França'
 
-from PyQt5.QtCore import QCoreApplication, QVariant
+from qgis.PyQt.QtCore import QVariant
 from qgis.core import (QgsProcessing,
                        QgsFeatureSink,
                        QgsProcessingException,
