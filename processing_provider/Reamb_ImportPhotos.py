2207c3bafccb0bce9a7e7eb8287991b17279e629
Leandro França <geoleandro.franca@gmail.com>
2025-10-17 17:27:16 -0300

    Update Reamb_ImportPhotos.py

--- processing_provider/Reamb_ImportPhotos.py
+++ processing_provider/Reamb_ImportPhotos.py
@@ -15,8 +15,7 @@ __author__ = 'Leandro França'
 __date__ = '2021-02-16'
 __copyright__ = '(C) 2021, Leandro França'
 
-from qgis.PyQt.QtCore import QCoreApplication
-from PyQt5.QtCore import QVariant
+from qgis.PyQt.QtCore import QVariant
 from qgis.core import (QgsApplication,
                        QgsGeometry,
                        QgsPoint,
