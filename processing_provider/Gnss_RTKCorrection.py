138b3752feb0eac60c6ca5f1b2e4dbac43224abc
Leandro França <geoleandro.franca@gmail.com>
2025-10-17 17:26:20 -0300

    Update Gnss_RTKCorrection.py

--- processing_provider/Gnss_RTKCorrection.py
+++ processing_provider/Gnss_RTKCorrection.py
@@ -15,7 +15,7 @@ __author__ = 'Leandro França'
 __date__ = '2022-06-13'
 __copyright__ = '(C) 2022, Leandro França'
 
-from PyQt5.QtCore import QCoreApplication, QVariant
+from qgis.PyQt.QtCore import QVariant
 from qgis.core import (QgsProcessing,
                        QgsFeatureSink,
                        QgsWkbTypes,
