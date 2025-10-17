0cfa3aa753cc69a712a67fbb2d7b8332074ed746
Leandro França <geoleandro.franca@gmail.com>
2025-10-17 17:27:25 -0300

    Update Relief_DEMfilter.py

--- processing_provider/Relief_DEMfilter.py
+++ processing_provider/Relief_DEMfilter.py
@@ -15,7 +15,6 @@ __author__ = 'Leandro França'
 __date__ = '2021-12-18'
 __copyright__ = '(C) 2021, Leandro França'
 
-from PyQt5.QtCore import QCoreApplication, QVariant
 from qgis.core import (QgsProcessing,
                        QgsFeatureSink,
                        QgsWkbTypes,
