372571ad1b8dc0bb30f418443d12abe174a6425e
Leandro França <geoleandro.franca@gmail.com>
2025-10-17 17:28:04 -0300

    Update Vect_ReprojectGPKG.py

--- processing_provider/Vect_ReprojectGPKG.py
+++ processing_provider/Vect_ReprojectGPKG.py
@@ -15,7 +15,6 @@ __author__ = 'Leandro França'
 __date__ = '2024-10-19'
 __copyright__ = '(C) 2024, Leandro França'
 
-from PyQt5.QtCore import QCoreApplication, QVariant
 from qgis.core import (QgsProcessing,
                        QgsFeatureSink,
                        QgsWkbTypes,
