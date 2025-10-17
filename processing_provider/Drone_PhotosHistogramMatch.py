2368853f06cbe832885f935c14c1178a2acf50c9
Leandro França <geoleandro.franca@gmail.com>
2025-10-17 17:29:13 -0300

    Update Drone_PhotosHistogramMatch.py

--- processing_provider/Drone_PhotosHistogramMatch.py
+++ processing_provider/Drone_PhotosHistogramMatch.py
@@ -15,7 +15,6 @@ __author__ = 'Leandro França'
 __date__ = '2023-06-17'
 __copyright__ = '(C) 2023, Leandro França'
 
-from PyQt5.QtCore import QCoreApplication, QVariant
 from qgis.core import (QgsProcessing,
                        QgsFeatureSink,
                        QgsWkbTypes,
