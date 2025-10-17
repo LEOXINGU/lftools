642219cf1082c0fa82e9cfde139ee210ddc3b87a
Leandro França <geoleandro.franca@gmail.com>
2025-10-17 17:27:30 -0300

    Update Stat_confidenceEllipse.py

--- processing_provider/Stat_confidenceEllipse.py
+++ processing_provider/Stat_confidenceEllipse.py
@@ -15,7 +15,7 @@ __author__ = 'Leandro França'
 __date__ = '2020-08-22'
 __copyright__ = '(C) 2020, Leandro França'
 
-from PyQt5.QtCore import QCoreApplication, QVariant
+from qgis.PyQt.QtCore import QVariant
 from qgis.core import (QgsProcessing,
                        QgsFeatureSink,
                        QgsWkbTypes,
