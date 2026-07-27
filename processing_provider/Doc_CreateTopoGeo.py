516e2c3acb7c946decb570f5216930c71f6a416f
Leandro França <geoleandro.franca@gmail.com>
2026-07-27 11:55:30 -0300

    Update Doc_CreateTopoGeo.py

--- processing_provider/Doc_CreateTopoGeo.py
+++ processing_provider/Doc_CreateTopoGeo.py
@@ -16,6 +16,8 @@ __date__ = '2026-07-26'
 __copyright__ = '(C) 2026, Leandro França'
 
 import os
+import shutil
+import tempfile
 import processing
 
 from qgis.PyQt.QtGui import QIcon
@@ -58,7 +60,7 @@ class CreateTopoGeo(QgsProcessingAlgorithm):
         return 'documents'
 
     def tags(self):
-        return 'GeoOne,TopoGeo,demo,template,GeoPackage,GPKG,database,copy,reproject,CRS,GeoOne,TopoGeo,demonstração,modelo,GeoPackage,GPKG,banco de dados,copiar,reprojetar,SRC'.split(',')
+        return 'GeoOne,TopoGeo,demo,template,GeoPackage,GPKG,database,memorial,copy,reproject,CRS,GeoOne,TopoGeo,demonstração,modelo,GeoPackage,GPKG,banco de dados,copiar,reprojetar,SRC'.split(',')
 
     def icon(self):
         return QIcon(
@@ -76,59 +78,47 @@ class CreateTopoGeo(QgsProcessingAlgorithm):
         return super().flags() | QgsProcessingAlgorithm.FlagNoThreading
 
     txt_en = '''This tool creates a new GeoPackage file based on the simplified <b>TopoGeo Demo</b> template.
-
 <p>
 The generated database contains the minimum set of layers required to use the automated documentation tools available in the <b>LFTools</b> plugin, such as the generation of Survey Descriptions and other technical documents.
 </p>
-
 <h3>Generated layers</h3>
-
 <ul>
   <li><code>limit_point_p</code></li>
   <li><code>boundary_element_l</code></li>
   <li><code>property_area_a</code></li>
   <li><code>reference_point_p</code> — optional</li>
 </ul>
-
 <h3>Important notes</h3>
-
 <p>
 <b>1.</b> The <code>reference_point_p</code> layer is used exclusively to generate the <b>Geodetic mark report</b>.
 </p>
-
 <p>
-<b>2.</b> For the LFTools documentation tools to work correctly, the Coordinate Reference System (CRS) of the database must be <b>Geographic</b>.
+<b>2.</b> For the LFTools documentation tools to work correctly, the Coordinate Reference System (CRS) of the database <b>must be Geographic</b>.
 </p>
 '''
 
 
     txt_pt = '''Esta ferramenta cria um novo arquivo GeoPackage a partir do modelo simplificado <b>TopoGeo Demo</b>.
-
 <p>
 O banco de dados gerado contém o conjunto mínimo de camadas necessário para utilizar as ferramentas de documentação automatizada disponíveis no plugin <b>LFTools</b>, como a geração do Memorial Descritivo e de outras peças técnicas.
 </p>
-
 <h3>Camadas geradas</h3>
-
 <ul>
   <li><code>limit_point_p</code></li>
   <li><code>boundary_element_l</code></li>
   <li><code>property_area_a</code></li>
   <li><code>reference_point_p</code> — opcional</li>
 </ul>
-
 <h3>Observações importantes</h3>
-
 <p>
 <b>1.</b> A camada <code>reference_point_p</code> é utilizada exclusivamente para a geração da <b>Monografia do Marco Geodésico</b>.
 </p>
-
 <p>
-<b>2.</b> Para que as ferramentas de documentação do LFTools funcionem corretamente, o Sistema de Referência de Coordenadas (<b>SRC</b>) do banco de dados deve ser geográfico.
+<b>2.</b> Para que as ferramentas de documentação do LFTools funcionem corretamente, o Sistema de Referência de Coordenadas (SRC) do banco de dados <b>deve ser Geográfico</b>.
 </p>
 '''
 
-    figure = 'images/tutorial/vect_createTopoGeo.jpg'
+    figure = 'images/tutorial/doc_createTopoGeo.jpg'
 
     def shortHelpString(self):
         social_BW = Imgs().social_BW
@@ -138,7 +128,12 @@ O banco de dados gerado contém o conjunto mínimo de camadas necessário para u
                       <div align="right">
                       <p align="right">
                       <b>'''+self.tr('Author: Leandro Franca', 'Autor: Leandro França')+'''</b>
-                      </p>'''+ social_BW + '''</div>
+                      </p>
+                      <p align="right">
+                        <b><a href="'''+ self.tr('https://www.researchgate.net/publication/356911797_TopoGeo_a_data_model_for_elaboration_of_cadastral_survey_plans_and_land_register_documents',
+                        'https://geoone.com.br/ebooks/livro2/') + '''" target="_blank">'''+ self.tr('Click here for understanding more about this data model.',
+                                        'Clique aqui para entender mais sobre esse modelo de dados') +'</a><br><br>'+ self.tr('Author: Leandro Franca', 'Autor: Leandro França')+'''</b>
+                        </p>'''+ social_BW + '''</div>
                       '''
         return self.tr(self.txt_en, self.txt_pt) + footer
 
@@ -324,6 +319,89 @@ O banco de dados gerado contém o conjunto mínimo de camadas necessário para u
             'reference_point_p': 'TopoGeo_reference_point_p.qml',
         }
 
+        # Tradução do arquivo de estilos
+        dic_topogeo = {
+                        '[TYPE]': self.tr('type', 'tipo'),
+                        '[SEQUENCE]': self.tr('sequence', 'ordem'),
+                        '[CODE]': self.tr('code', 'código'),
+
+                        '[PARCEL]': self.tr('parcel', 'parcela'),
+
+                        '[M]': self.tr('Concrete monument with plate', 'Marco de concreto com chapa'),
+                        '[P]': self.tr('Type P (point)', 'Tipo P (ponto)'),
+                        '[V]': self.tr('Type V (virtual)', 'Tipo V (virtual)'),
+                        '[M_2]': self.tr('Concrete monument without plate', 'Marco de concreto sem chapa'),
+
+                        '[BORDERER]': self.tr('borderer', 'confrontante'),
+                        '[BORDERER_LABEL]': self.tr('borderer_label', 'confront_rotulo'),
+
+                        '[START_POINT_DESCR]': self.tr('start_point_description', 'descr_pnt_inicial'),
+
+                        '[AUTHORIZER]': self.tr('authorizer', 'responsavel'),
+                        '[AUTHORIZER_ID]': self.tr('authorizer_id', 'cpf_cnpj'),
+
+                        '[BORDERER_REGISTRY]': self.tr('borderer_registry', 'matricula'),
+
+                        '[PROPERTY]': self.tr('property', 'imóvel'),
+                        '[REGISTRY]': self.tr('registry', 'cadastro'),
+                        '[TRANSCRIPT]': self.tr('transcript', 'matricula'),
+
+                        '[OWNER]': self.tr('owner', 'proprietário'),
+                        '[OWNER_ID]': self.tr('owner_id', 'CPF do proprietário'),
+
+                        '[ADDRESS]': self.tr('address', 'endereço'),
+                        '[COUNTY]': self.tr('county', 'município'),
+                        '[STATE]': self.tr('state', 'UF'),
+
+                        '[SURVEY_DATE]': self.tr('survey_date', 'levantamento - data'),
+                        '[SURVEYOR]': self.tr('surveyor', 'agrimensor'),
+
+                        '[TECH_MANAGER]': self.tr('Technical Manager', 'Responsável Técnico (RT)'),
+                        '[PROF_ID]': self.tr('Professional registration', 'Registro profissional do RT'),
+
+                        '[GEODETIC_MARK]': self.tr('GEODETIC MARK', 'MARCO GEODÉSICO'),
+                        '[DESCRIPTION]': self.tr('Description', 'Descrição'),
+
+                        '[ELLIP_HEIGHT]': self.tr('Ellipsoidal height', 'altitude geométrica'),
+                        '[ORTHO_HEIGHT]': self.tr('Orthometric height', 'altitude física'),
+
+                        '[INSTRUMENT]': self.tr('GNSS receiver', 'receptor GNSS'),
+                        '[SURVEY_METHOD]': self.tr('Survey method', 'método de levantamento'),
+                        '[RELATIVE_STATIC]': self.tr('Static Relative', 'Relativo Estático'),
+                        '[SURVEY_REF_BASE]': self.tr('Reference base', 'Base de referência'),
+
+                        '[OBSERVATION]': self.tr('Observation', 'Observação'),
+
+                        '[SURVEY_RESP]': self.tr('Survey responsible', 'levantamento - responsável'),
+                        '[PROC_DATA]': self.tr('Processing date', 'processamento - data'),
+                        '[PROC_RESP]': self.tr('Processing responsible', 'processamento - responsável'),
+                        '[REPORT_DATA]': self.tr('Report date', 'monografia - data'),
+                        '[REPORT_RESP]': self.tr('Report responsible', 'monografia - responsável'),
+
+                        '[MARK_PHOTO]': self.tr('Mark photo', 'foto do marco'),
+                        '[PAN_PHOTO]': self.tr('Panoramic photo', 'foto panorâmica'),
+                        '[AERIAL_PHOTO]': self.tr('Aerial image', 'imagem aérea'),
+
+                        '[PROFESSION]': self.tr('Professional qualification', 'formação profissional'),
+                        '[PROFESSION_ID]': self.tr('Professional registration', 'registro profissional'),
+
+                        '[COORDINATES]': self.tr('Coordinates', 'Coordenadas'),
+                        '[METHODOLOGY]': self.tr('Methodology', 'Metodologia'),
+                        '[REF_BASE]': self.tr('Reference Base(s)', 'Base(s) de Referência'),
+                        '[PHASES]': self.tr('Phases', 'Etapas'),
+                        '[IMAGES]': self.tr('Images', 'Imagens'),
+                        '[RESPONSIBILITY]': self.tr('Responsibility', 'Responsabilidade'),
+
+                        '[ALTIMETRIC]': self.tr('Altimetric', 'Altimétrico'),
+                        '[PLANIMETRIC]': self.tr('Planimetric', 'Planimétrico'),
+                        '[PLANIALTIMETRIC]': self.tr('Planialtimetric', 'Planialtimétrico'),
+                        '[GRAVIMETRIC]': self.tr('Gravimetric', 'Gravimétrico'),
+                        '[OTHER]': self.tr('Other', 'Outro'),
+                    }
+        
+        chaves = list(dic_topogeo.keys())
+        valores = list(dic_topogeo.values())
+
         # Posição onde o grupo será inserido na árvore de camadas.
         insert_index = 0
 
@@ -364,11 +442,18 @@ O banco de dados gerado contém o conjunto mínimo de camadas necessário para u
             # Identifica o arquivo de estilo correspondente à camada.
             style_filename = layer_styles.get(layer_name)
 
+
             if style_filename:
                 style_path = os.path.join(
                     styles_directory,
                     style_filename
                 )
+
+                style_path = self.prepare_temp_qml(
+                        style_path,
+                        chaves,
+                        valores
+                    )
 
                 if os.path.isfile(style_path):
 
@@ -467,3 +552,35 @@ O banco de dados gerado contém o conjunto mínimo de camadas necessário para u
         )
 
         return {self.OUTPUT: created_gpkg}
+
+
+    def prepare_temp_qml(self, qml_path, tokens_to_replace, text_to_replace):
+            """
+            Cria um QML temporário substituindo múltiplos placeholders por textos.
+            """
+            if not os.path.isfile(qml_path):
+                raise FileNotFoundError(f"QML not found: {qml_path}!")
+    
+            if not isinstance(tokens_to_replace, (list, tuple)) or not isinstance(text_to_replace, (list, tuple)):
+                raise TypeError("tokens_to_replace and text_to_replace must be list or tuple!")
+    
+            if len(tokens_to_replace) != len(text_to_replace):
+                raise ValueError("tokens_to_replace and text_to_replace must have the SAME length!")
+    
+            with open(qml_path, 'r', encoding='utf-8') as f:
+                content = f.read()
+    
+            for token, txt in zip(tokens_to_replace, text_to_replace):
+                content = content.replace(token, txt)
+    
+            temp_dir = tempfile.mkdtemp(prefix='lftools_qml_')
+            temp_qml_path = os.path.join(temp_dir, os.path.basename(qml_path))
+    
+            try:
+                with open(temp_qml_path, 'w', encoding='utf-8') as f:
+                    f.write(content)
+            except Exception as e:
+                shutil.rmtree(temp_dir, ignore_errors=True)
+                raise RuntimeError(f"Error saving temporary QML: {e}")
+    
+            return temp_qml_path
