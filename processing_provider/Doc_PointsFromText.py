d86bfb8a3f8f3c1f283ea613d39379447cc43b4c
Leandro França <geoleandro.franca@gmail.com>
2026-08-25 10:37:12 -0300

    Update Doc_PointsFromText.py

--- processing_provider/Doc_PointsFromText.py
+++ processing_provider/Doc_PointsFromText.py
@@ -6,7 +6,7 @@
     ---------------------
     Date                 : Jun 12
     Copyright            : (C) 2022 by Leandro França
-    Email                : geoleandro.franca@gmail.com
+    Email                : contato@geoone.com.br
 ***************************************************************************
 *                                                                         *
 *   This program is free software; you can redistribute it and/or modify  *
