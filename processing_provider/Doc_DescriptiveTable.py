c56348a5fc4e90edd78aace7e024292d18a53e56
Leandro França <geoleandro.franca@gmail.com>
2026-08-25 10:37:21 -0300

    Update Doc_DescriptiveTable.py

--- processing_provider/Doc_DescriptiveTable.py
+++ processing_provider/Doc_DescriptiveTable.py
@@ -6,7 +6,7 @@
     ---------------------
     Date                 : Jul 10
     Copyright            : (C) 2020 by Leandro França
-    Email                : geoleandro.franca@gmail.com
+    Email                : contato@geoone.com.br
 ***************************************************************************
 *                                                                         *
 *   This program is free software; you can redistribute it and/or modify  *
